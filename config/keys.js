29a1615929c5c9c215c00f425c4877871d0bade5
Alex Lerner <alernerdev@gmail.com>
2018-07-21 11:19:14 -0400

    debugging

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,7 @@
 // determine if we are using prod or dev credentials
 
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./dev');
-    // module.exports = require('./prod');
+    module.exports = require('./prod');
 } else {
     module.exports = require('./dev');
 }
--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,12 @@
 // prod.js -- production keys
 module.exports = {
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientID: '605235560369-i7u4v871cboa704fnlquf0opl4h4i13c.apps.googleusercontent.com',
+    googleClientSecret: 'c0liX63K0EJrIKuOW6qs_MWw',
+    mongoURI: 'mongodb://alernerdev:password@ds231090.mlab.com:31090/emaily-dev',
+	cookieKey: 'blahalernerdev@gmail.comblah'
+
+    /*googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY
+    cookieKey: process.env.COOKIE_KEY*/
 }
